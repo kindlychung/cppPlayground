0e9754bb18968f49b2596f06c92898429c9372db
Victor Chung <kindlychung@gmail.com>
2014-08-06 21:49:33 +0200

    ignore

--- staticlib/src/ncols.d
+++
@@ -1,5 +0,0 @@
-src/ncols.d: ../src/ncols.cpp ../src/ncols.h ../src/fileExists.h
-
-../src/ncols.h:
-
-../src/fileExists.h:
--- staticlib/src/printlines.d
+++
@@ -1,3 +0,0 @@
-src/printlines.d: ../src/printlines.cpp ../src/printlines.h
-
-../src/printlines.h:
